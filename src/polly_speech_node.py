7444f104657294777902072f5320ba761236f157
Eddie <ehwa009@aucklanduni.ac.nz>
2018-11-25 18:19:06 +1300

    Simultaneous  multi lang supprot-temp

--- src/polly_speech_node.py
+++ src/polly_speech_node.py
@@ -9,10 +9,12 @@ import pyaudio
 import tempfile
 import threading
 import json
-from ctypes import *
 import contextlib
+
+from ctypes import *
 from std_msgs.msg import String, Bool
 from polly_speech.msg import SpeechAction, SpeechResult, SpeechFeedback
+from langdetect import detect
 
 VOWELS = ['@', 'a', 'e', 'E', 'i', 'o', 'O', 'u']
 
@@ -46,10 +48,15 @@ class PollySpeechNode:
             region_name=data['region'],
             aws_access_key_id=data['aws_access_key_id'],
             aws_secret_access_key=data['aws_secret_access_key'],
-            endpoint_url="https://polly.us-east-1.amazonaws.com")
+            endpoint_url="https://polly.us-west-1.amazonaws.com")
 
         try:
             self.lang = data['lang']
+            if self.lang == 'en-US':
+                self.lang_condition = ['en', 'no']
+            elif self.lang == 'ko-KR':
+                self.lang_condition = ['ko']
+            
             self.voice = data['voice']
             self.default_pitch = data['default_pitch']
         except KeyError as e:
@@ -73,58 +80,62 @@ class PollySpeechNode:
         feedback = SpeechFeedback()
 
         msg_text = goal.text
-        speech_text = ''
-        if msg_text.startswith('$'):
-            msg_text = msg_text.strip('$')
-            lang_code, text_body = msg_text.split('|')
+        
+        # check language
+        lang = detect(msg_text.decode('utf-8'))
+        print('detected lang: %s' %lang)
+        if lang in self.lang_condition:
+            speech_text = ''
+            if msg_text.startswith('$'):
+                msg_text = msg_text.strip('$')
+                lang_code, text_body = msg_text.split('|')
+                speech_text = '<speak><prosody rate="medium" pitch="%s">'%self.default_pitch + '<lang xml:lang="%s">'%lang_code + text_body + '</lang></prosody></speak>'
+            else:
+                speech_text = '<speak><prosody rate="medium" pitch="%s">'%self.default_pitch + msg_text + '</prosody></speak>'
 
-            speech_text = '<speak><prosody rate="medium" pitch="%s">'%self.default_pitch + '<lang xml:lang="%s">'%lang_code + text_body + '</lang></prosody></speak>'
-        else:
-            speech_text = '<speak><prosody rate="medium" pitch="%s">'%self.default_pitch + msg_text + '</prosody></speak>'
+            resp = self.client.synthesize_speech(OutputFormat="json", Text=speech_text, SpeechMarkTypes=['viseme'], TextType="ssml", VoiceId=self.voice)
+            with open(tempfile.gettempdir() + '/polly_wave.txt', 'w') as f:
+                f.write(resp['AudioStream'].read())
+                f.close()
 
-        resp = self.client.synthesize_speech(OutputFormat="json", Text=speech_text, SpeechMarkTypes=['viseme'], TextType="ssml", VoiceId=self.voice)
-        with open(tempfile.gettempdir() + '/polly_wave.txt', 'w') as f:
-            f.write(resp['AudioStream'].read())
-            f.close()
+            viseme_data = []
+            with open(tempfile.gettempdir() + '/polly_wave.txt', 'r') as f:
+                for line in f:
+                    viseme = json.loads(line)
+                    if viseme['value'] in VOWELS:
+                        viseme_data.append([viseme['value'], viseme['time']])
 
-        viseme_data = []
-        with open(tempfile.gettempdir() + '/polly_wave.txt', 'r') as f:
-            for line in f:
-                viseme = json.loads(line)
-                if viseme['value'] in VOWELS:
-                    viseme_data.append([viseme['value'], viseme['time']])
+            resp = self.client.synthesize_speech(OutputFormat="pcm", Text=speech_text, TextType="ssml", VoiceId=self.voice)
+            with open(tempfile.gettempdir() + '/polly_wave.wav', 'w') as f:
+                f.write(resp['AudioStream'].read())
+                f.close()
 
-        resp = self.client.synthesize_speech(OutputFormat="pcm", Text=speech_text, TextType="ssml", VoiceId=self.voice)
-        with open(tempfile.gettempdir() + '/polly_wave.wav', 'w') as f:
-            f.write(resp['AudioStream'].read())
-            f.close()
+            self.pub_status.publish(True)
 
-        self.pub_status.publish(True)
+            stream = self.p.open(
+                format=self.p.get_format_from_width(2),
+                channels=1,
+                rate=16000,
+                output=True)
 
-        stream = self.p.open(
-            format=self.p.get_format_from_width(2),
-            channels=1,
-            rate=16000,
-            output=True)
-
-        th = threading.Thread(target=self.handle_pub_viseme, args=(viseme_data,))
-        th.start()
-        with open(tempfile.gettempdir() + '/polly_wave.wav', 'rb') as pcm_file:
-            data = pcm_file.read(100)
-
-            while data != '':
-                stream.write(data)
+            th = threading.Thread(target=self.handle_pub_viseme, args=(viseme_data,))
+            th.start()
+            with open(tempfile.gettempdir() + '/polly_wave.wav', 'rb') as pcm_file:
                 data = pcm_file.read(100)
-                feedback.is_speaking = True
-                self.speech_server.publish_feedback(feedback)
-        rospy.sleep(0.2)
-        th.join()
 
-        stream.stop_stream()
-        stream.close()
+                while data != '':
+                    stream.write(data)
+                    data = pcm_file.read(100)
+                    feedback.is_speaking = True
+                    self.speech_server.publish_feedback(feedback)
+            rospy.sleep(0.2)
+            th.join()
 
-        self.pub_vowels.publish('N')
-        self.pub_status.publish(False)
+            stream.stop_stream()
+            stream.close()
+
+            self.pub_vowels.publish('N')
+            self.pub_status.publish(False)
 
         result.result = True
         self.speech_server.set_succeeded(result)
